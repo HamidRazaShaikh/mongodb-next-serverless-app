4d0cd6c4558390bfb5a7d992d58351438e69b37b
HamidRazaVision <hamidraza.chemistry@gmail.com>
2023-01-11 10:47:45 -0800

    url changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
     MONGO_URI: 'mongodb+srv://hamid:hamid@foodapi.lbfuc2a.mongodb.net/?retryWrites=true&w=majority',
-    BASE_URL : 'http://localhost:3000',
+    BASE_URL : 'https://mongodb-next-serverless-app.vercel.app',
   },
 }
